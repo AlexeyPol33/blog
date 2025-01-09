cce8227597f98c6038246eb949c367c3a08a40d0
Alexey Poltavskiy <79776177342@yandex.ru>
2025-01-09 21:28:30 +0300

    add rss

--- blog/blog_app/urls.py
+++ blog/blog_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .feeds import LatestPostsFeed
 
 
 app_name = 'blog_app'
@@ -11,5 +12,7 @@ urlpatterns = [
          views.post_detail,
          name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
-    path('<int:post_id>/comment/', views.post_comment,name='post_comment')
+    path('<int:post_id>/comment/', views.post_comment,name='post_comment'),
+    path ('feed/', LatestPostsFeed(), name='post_feed'),
+
 ]
